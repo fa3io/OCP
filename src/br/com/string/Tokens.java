87161b3a5dc7a2fbfa467fca904f3638bbf831bf
Fábio Santos Alves <fa3io.santos.alves@gmail.com>
2017-01-18 19:00:47 -0200

    Atualização em explicação
    Atualização em explicação de como funciona a tokenização

--- src/br/com/string/Tokens.java
+++ src/br/com/string/Tokens.java
@@ -13,8 +13,15 @@ public class Tokens {
 		 String str = "tokens\twith tabs and \r\nnew lines.";
 		 StringTokenizer st = new StringTokenizer(str);
 		 System.out.println(st.countTokens());
-		
+		 
+		 
+		 //No construtor entra a String a ser analisada e o delimitador
+		 StringTokenizer tokens = new StringTokenizer("aa a aaaa", "a");
+		 System.out.println(tokens.countTokens());
 	
+		 while (tokens.hasMoreTokens()) {
+			 System.out.println(tokens.nextToken());
+		 }
 		
 	}
 
